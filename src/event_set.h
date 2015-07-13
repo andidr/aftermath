5eb1f65692ac2130b614a0925df15d219c048985
Andi Drebes <hackbert@drebesium.org>
2015-07-13 15:06:28 +0200

    Remove unused function event_set_get_major_owner_node_in_interval

--- src/event_set.h
+++ src/event_set.h
@@ -79,7 +79,6 @@ void event_set_sort_annotations(struct event_set* es);
 int event_set_get_first_counter_event_in_interval(struct event_set* es, uint64_t counter_id, uint64_t start, uint64_t end);
 int event_set_compare_cpus(const void* p1, const void* p2);
 uint64_t event_set_get_average_task_length_in_interval(struct event_set* es, struct filter* f, long double* num_tasks, uint64_t start, uint64_t end);
-int event_set_get_major_owner_node_in_interval(struct event_set* es, struct filter* f, uint64_t start, uint64_t end, int max_numa_node, int* major_node);
 int event_set_get_major_written_node_in_interval(struct event_set* es, struct filter* f, uint64_t start, uint64_t end, int max_numa_node, int* major_node);
 int event_set_get_major_read_node_in_interval(struct event_set* es, struct filter* f, uint64_t start, uint64_t end, int max_numa_node_id, int* major_node);
 int event_set_get_major_accessed_node_in_interval(struct event_set* es, struct filter* f, uint64_t start, uint64_t end, int max_numa_node_id, int* major_node);
