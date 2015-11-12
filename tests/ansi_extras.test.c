a95504c619da1e249650e462264171359a258853
Andi Drebes <andi.drebes@lip6.fr>
2015-11-12 15:46:12 +0000

    Add unit tests for unescape_string

--- tests/ansi_extras.test.c
+++ tests/ansi_extras.test.c
@@ -133,6 +133,117 @@ UNIT_TEST(escape_string_test)
 }
 END_TEST()
 
+struct twostrs_v {
+	const char* r;
+	const char* e;
+	const int v;
+};
+
+static const struct twostrs_v exp_unescape[] = {
+	{"AAA", "AAA", 0},
+	{"A\\\"", "A\"", 0},
+	{"A\\t\\tA", "A\t\tA", 0},
+	{"A\\a\\b\\f\\n\\r\\t\\v\\\\\\\"A", "A\a\b\f\n\r\t\v\\\"A", 0},
+	{"String with funky\\x01\\x02\\x01 chars", "String with funky\x01\x02\x01 chars", 0},
+	{"String with funky\\001\\002\\001 chars", "String with funky\x01\x02\x01 chars", 0},
+	{"Some \\\" quotes with \\\"   spaces    \\\"   ", "Some \" quotes with \"   spaces    \"   ", 0},
+	{"\\x", "", 1},
+	{"\\xA", "\x0A", 0},
+	{"\\xAG", "\xAG", 0},
+	{"\\xAB", "\xAB", 0},
+	{"\\xab", "\xAB", 0},
+	{"\\xAb", "\xAB", 0},
+	{"\\xaB", "\xAB", 0},
+	{"\\1", "\1", 0},
+	{"\\01", "\01", 0},
+	{"\\008", "", 0},
+	{"\\009", "", 0},
+	{"\\c", "", 1},
+	{"\\d", "", 1},
+	{"\\e", "", 1},
+	{"\\g", "", 1},
+	{"\\h", "", 1},
+	{"\\i", "", 1},
+	{"\\j", "", 1},
+	{"\\k", "", 1},
+	{"\\l", "", 1},
+	{"\\m", "", 1},
+	{"\\o", "", 1},
+	{"\\p", "", 1},
+	{"\\q", "", 1},
+	{"\\s", "", 1},
+	{"\\u", "", 1},
+	{"\\w", "", 1},
+	{"\\x", "", 1},
+	{"\\y", "", 1},
+	{"\\z", "", 1},
+	{"\\C", "", 1},
+	{"\\D", "", 1},
+	{"\\E", "", 1},
+	{"\\G", "", 1},
+	{"\\H", "", 1},
+	{"\\I", "", 1},
+	{"\\J", "", 1},
+	{"\\K", "", 1},
+	{"\\L", "", 1},
+	{"\\M", "", 1},
+	{"\\O", "", 1},
+	{"\\P", "", 1},
+	{"\\Q", "", 1},
+	{"\\S", "", 1},
+	{"\\U", "", 1},
+	{"\\W", "", 1},
+	{"\\X", "", 1},
+	{"\\Y", "", 1},
+	{"\\Z", "", 1},
+	{"\\0", "", 0},
+	{"\\1", "\1", 0},
+	{"\\2", "\2", 0},
+	{"\\3", "\3", 0},
+	{"\\4", "\4", 0},
+	{"\\5", "\5", 0},
+	{"\\6", "\6", 0},
+	{"\\7", "\7", 0},
+	{"\\8", "", 1},
+	{"\\9", "", 1},
+	{NULL, NULL, 0}
+};
+
+UNIT_TEST(unescape_string_in_place_test)
+{
+	char *tmp;
+
+	for(const struct twostrs_v* i = &exp_unescape[0]; i->r; i++) {
+		tmp = strdup(i->r);
+		ASSERT_NONNULL(tmp);
+		ASSERT_EQUALS(unescape_string_in_place(&tmp, 1), i->v);
+
+		if(i->v == 0)
+			ASSERT_EQUALS_STRING(tmp, i->e);
+
+		free(tmp);
+	}
+}
+END_TEST()
+
+UNIT_TEST(unescape_string_test)
+{
+	char *tmp;
+
+	for(const struct twostrs_v* i = &exp_unescape[0]; i->r; i++) {
+		tmp = unescape_string(i->r);
+
+		if(i->v == 0) {
+			ASSERT_NONNULL(tmp);
+			ASSERT_EQUALS_STRING(tmp, i->e);
+			free(tmp);
+		} else {
+			ASSERT_NULL(tmp);
+		}
+	}
+}
+END_TEST()
+
 UNIT_TEST_SUITE(ansi_extras_test)
 {
 	ADD_TEST(strreplace_test);
@@ -140,5 +251,7 @@ UNIT_TEST_SUITE(ansi_extras_test)
 	ADD_TEST(int32_swap_test);
 	ADD_TEST(int16_swap_test);
 	ADD_TEST(escape_string_test);
+	ADD_TEST(unescape_string_in_place_test);
+	ADD_TEST(unescape_string_test);
 }
 END_TEST_SUITE()
