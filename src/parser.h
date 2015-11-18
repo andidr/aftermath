2d3a7e50b58c96bb496b9f8da8d439651b1a90f5
Andi Drebes <andi.drebes@lip6.fr>
2015-11-18 19:19:24 +0000

    Add function parser_read_int_with_unit

--- src/parser.h
+++ src/parser.h
@@ -315,6 +315,51 @@ static inline int parser_read_next_int(struct parser* p, struct parser_token* t)
 	return parser_read_int(p, t);
 }
 
+/* Consumes all numeric characters from the current position. An
+ * optional unit prefix at the end is allowed (i.e., K, M, G, T,
+ * P). If no such sequence exists the function returns 1. Otherwise,
+ * it sets the token to the sequence and returns 0.
+ */
+static inline int parser_read_int_with_unit(struct parser* p, struct parser_token* t)
+{
+	if(parser_reached_end(p))
+		return 1;
+
+	if(!isdigit(*p->curr))
+		return 1;
+
+	t->str = p->curr;
+	t->len = 1;
+	p->curr++;
+
+	while(p->curr != p->end && isdigit(*p->curr)) {
+		t->len++;
+		p->curr++;
+	}
+
+	if(!parser_reached_end(p)) {
+		if(*p->curr == 'K' ||
+		   *p->curr == 'M' ||
+		   *p->curr == 'G' ||
+		   *p->curr == 'T' ||
+		   *p->curr == 'P')
+		{
+			t->len++;
+			p->curr++;
+		}
+	}
+
+	return 0;
+}
+
+/* Same as parser_read_int_with_unit, but skips all preceding
+ * whitespace */
+static inline int parser_read_next_int_with_unit(struct parser* p, struct parser_token* t)
+{
+	parser_skip_ws(p);
+	return parser_read_int_with_unit(p, t);
+}
+
 /* Returns the address of the next occurrence of c. The position of
  * the parser is advanced to the position of the character. If the
  * character is not found NULL is returned. */
