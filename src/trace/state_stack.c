32b5ca8f57a0ff81933d4084bb56068141502368
Andi Drebes <andi.drebes@lip6.fr>
2016-03-29 12:43:00 +0100

    libaftermath-trace: Add auxiliary data structure for state stacks

---
+++ src/trace/state_stack.c
@@ -0,0 +1,132 @@
+/**
+ * Copyright (C) 2016 Andi Drebes <andi.drebes@lip6.fr>
+ *
+ * Libaftermath-trace is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+#include "state_stack.h"
+
+/**
+ * Initialize a state stack
+ * @param max_entries Maximum depth of the stack
+ * @return 0 on success, 1 otherwise
+ */
+int am_state_stack_init(struct am_state_stack* s, size_t max_entries)
+{
+	s->size = max_entries;
+	s->top = 0;
+
+	if(!(s->entries = malloc(s->size * sizeof(*s->entries))))
+		return 1;
+
+	return 0;
+}
+
+/**
+ * Free all resources associated to a state stack
+ */
+void am_state_stack_destroy(struct am_state_stack* s)
+{
+	free(s->entries);
+}
+
+/**
+ * Push a new state onto the stack. The timestamp of the entry is the
+ * current timestamp.
+ * @param state The ID of the newly entered state
+ * @return 0 on success, 1 otherwise
+ */
+int am_state_stack_push(struct am_state_stack* s, am_state_t state)
+{
+	if(s->top >= s->size)
+		return 1;
+
+	s->entries[s->top].state = state;
+	s->entries[s->top].start = am_timestamp_now();
+
+	s->top++;
+
+	return 0;
+}
+
+/**
+ * Push a new state onto the stack and create a new state event in an
+ * event set. The timestamp for the start of the event is the
+ * timestamp of the moment when the state on the top of the stack was
+ * entered. The timestamp for the end is the current timestamp.
+ *
+ * If the stack is empty, no event is traced.
+ *
+ * @param state The ID of the newly entered state
+ * @return 0 on success, 1 otherwise
+ */
+int am_state_stack_push_trace(struct am_state_stack* s,
+			      struct am_event_set* es,
+			      am_state_t state)
+{
+	if(am_state_stack_push(s, state))
+		return 1;
+
+	if(s->top > 1) {
+		if(am_event_set_trace_state(es, s->entries[s->top-2].state,
+					    s->entries[s->top-2].start,
+					    am_timestamp_now()))
+		{
+			return 1;
+		}
+	}
+
+	return 0;
+}
+
+/**
+ * Pop a state from the stack.
+ * @return 0 on success, 1 otherwise (e.g., if the stack is already empty)
+ */
+int am_state_stack_pop(struct am_state_stack* s)
+{
+	if(s->top == 0)
+		return 1;
+
+	s->top--;
+
+	if(s->top > 0)
+		s->entries[s->top-1].start = am_timestamp_now();
+
+	return 0;
+}
+
+/**
+ * Pop a state from the stack and create a new state event in an event
+ * set. The timestamp for the start of the event is the timestamp of
+ * the moment when the state on the top of the stack was entered. The
+ * timestamp for the end is the current timestamp.
+ *
+ * @param es The event set in which the event should be traced
+ * @return 0 on success, 1 otherwise
+ */
+int am_state_stack_pop_trace(struct am_state_stack* s, struct am_event_set* es)
+{
+	if(s->top == 0)
+		return 1;
+
+	if(am_event_set_trace_state(es, s->entries[s->top-1].state,
+				    s->entries[s->top-1].start,
+				    am_timestamp_now()))
+	{
+		return 1;
+	}
+
+	return am_state_stack_pop(s);
+}
---
+++ src/trace/state_stack.h
@@ -0,0 +1,60 @@
+/**
+ * Copyright (C) 2016 Andi Drebes <andi.drebes@lip6.fr>
+ *
+ * Libaftermath-trace is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+#ifndef AFTERMATH_STATE_STACK_H
+#define AFTERMATH_STATE_STACK_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "trace.h"
+
+/* Single entry on a state stack */
+struct am_state_stack_entry {
+	/* Id of the associated state */
+	am_state_t state;
+
+	/* Timestamp of the moment when the state was entered */
+	am_timestamp_t start;
+};
+
+struct am_state_stack {
+	/* Next position */
+	size_t top;
+
+	/* Maximal depth of the stack */
+	size_t size;
+
+	/* Actual stack entries */
+	struct am_state_stack_entry* entries;
+};
+
+int am_state_stack_init(struct am_state_stack* s, size_t max_entries);
+void am_state_stack_destroy(struct am_state_stack* s);
+int am_state_stack_push(struct am_state_stack* s, am_state_t state);
+int am_state_stack_push_trace(struct am_state_stack* s,
+			      struct am_event_set* es,
+			      am_state_t state);
+int am_state_stack_pop(struct am_state_stack* s);
+int am_state_stack_pop_trace(struct am_state_stack* s, struct am_event_set* es);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
