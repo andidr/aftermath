8402f35142c8318df703cc67433f5916e1dbe508
Andi Drebes <andi@drebesium.org>
2017-10-10 16:20:55 +0200

    Libaftermath-core: Add type definition files

---
+++ refactor/libaftermath-core/src/defs/am_dsk_types.py
@@ -0,0 +1,342 @@
+#!/usr/bin/env python
+
+# Author: Andi Drebes <andi.drebes@lip6.fr>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301,
+# USA.
+
+import am_type_aux
+import am_base_types
+
+# Definition of in-memory data structures. See doc/type_definitions for
+# documentation.
+
+__default_defs = ["type", "dsk_read", "dsk_write"]
+
+__default_fields = {
+    "compound" : True,
+    "destructor" : None,
+    "needs_constructor" : False,
+    "defs" : __default_defs,
+    "is_frame" : True
+}
+
+# Header at the beginning of a trace file
+header = {
+    "am_dsk_header" : {
+        "type_id" : None,
+        "is_frame" : False,
+        "fields" : [
+            {"name" : "magic",
+             "type" : "uint32_t",
+             "comment" : "Magic number for Aftermath trace files"},
+            {"name" : "version",
+             "type" : "uint32_t",
+             "comment" : "Version of the trace format" }
+        ]
+    }
+}
+
+# Header of each frame
+type_fields = [ { "name" : "type",
+                  "type" : "uint32_t",
+                  "comment" : "Type of this frame"} ]
+
+# Header for each frame that defines something global for a trace
+global_frame_fields = type_fields
+
+# Header for each frame that defines an event associated to an event collection
+event_frame_fields = type_fields + [
+    { "name" : "collection_id",
+      "type" : "uint32_t",
+      "comment" : "ID of the event collection this event belongs to"} ]
+
+frames = {
+    "am_dsk_hierarchy_description" : {
+        "type_id" : "AM_FRAME_TYPE_HIERARCHY_DESCRIPTION",
+        "assert" : False,
+        "entity" : "hierarchy description",
+        "fields" : global_frame_fields + [
+            {"name" : "id",
+             "type" : "uint32_t",
+             "comment" : "Numerical ID of this hierarchy"},
+            {"name" : "name",
+             "type" : "am_dsk_string",
+             "comment" : "Name of the hierarchy" }]
+    },
+
+    "am_dsk_hierarchy_node" : {
+        "type_id" : "AM_FRAME_TYPE_HIERARCHY_NODE",
+        "assert" : False,
+        "entity" : "hierarchy node",
+        "fields" : global_frame_fields + [
+            {"name" : "hierarchy_id",
+             "type" : "uint32_t",
+             "comment" : "Numerical ID of the hierarchy this node belongs to"},
+            {"name" : "id",
+             "type" : "uint32_t",
+             "comment" : "Numerical ID of this hierarchy node"},
+            {"name" : "parent_id", # Special value: 0 -> Does not have a parent
+             "type" : "uint32_t",
+             "comment" : "Numerical ID of the parent of this node"},
+            {"name" : "name",
+             "type" : "am_dsk_string",
+             "comment" : "Name of this node" }]
+    },
+
+    "am_dsk_state_description" : {
+        "type_id" : "AM_FRAME_TYPE_STATE_DESCRIPTION",
+        "assert" : False,
+        "entity" : "state description",
+        "defs" : ["dsk_to_mem_copy_function"] + __default_defs,
+        "fields" : global_frame_fields + [
+            {"name" : "state_id",
+             "type" : "uint32_t",
+             "comment" : "Numerical ID of the state"},
+            {"name" : "name",
+             "type" : "am_dsk_string",
+             "comment" : "Name of the state" }],
+        "process" : {
+            "type" : "per_trace_array",
+            "args" : {
+                "trace_array_field" : "state_descriptions",
+                "trace_array_struct_name" : "am_state_description_array",
+                "mem_struct_name" : "am_state_description",
+                "dsk_struct_sort_field" : "state_id",
+                "dsk_to_mem_function" : "am_dsk_state_description_to_mem"
+            },
+        },
+        "to_mem_copy_fields" : ["state_id", "name"]
+    },
+
+    "am_dsk_state_event" : {
+        "type_id" : "AM_FRAME_TYPE_STATE_EVENT",
+        "assert" : True,
+        "defs" : ["dsk_to_mem_copy_function"] + __default_defs,
+        "entity" : "state event",
+        "fields" : event_frame_fields + [
+            {"name" : "state",
+             "type" : "uint32_t",
+             "comment" : "Numerical ID of the state"},
+            {"name" : "interval",
+             "type" : "am_dsk_interval",
+             "comment" : "Interval during which the state was active" }],
+        "process" : {
+            "type" : "per_event_collection_interval",
+            "args" : {
+                "ecoll_array_type_id" : "AM_EVENT_ARRAY_TYPE_STATE_EVENT",
+                "ecoll_array_struct_name" : "am_state_event_array",
+                "mem_struct_name" : "am_state_event",
+                "mem_struct_interval_field" : "interval",
+                "dsk_struct_interval_field" : "interval",
+                "dsk_struct_ecoll_id_field" : "collection_id",
+                "dsk_to_mem_function" : "am_dsk_state_event_to_mem"
+            }
+        },
+        "to_mem_copy_fields" : ["state", "interval"]
+    },
+
+    "am_dsk_counter_description" : {
+        "type_id" : "AM_FRAME_TYPE_COUNTER_DESCRIPTION",
+        "assert" : False,
+        "entity" : "counter description",
+        "defs" : ["dsk_to_mem_copy_function"] + __default_defs,
+        "fields" : global_frame_fields + [
+            {"name" : "counter_id",
+             "type" : "uint32_t",
+             "comment" : "Numerical ID of the counter"},
+            {"name" : "name",
+             "type" : "am_dsk_string",
+             "comment" : "Name of the counter" }],
+        "process" : {
+            "type" : "per_trace_array",
+            "args" : {
+                "trace_array_field" : "counter_descriptions",
+                "trace_array_struct_name" : "am_counter_description_array",
+                "mem_struct_name" : "am_counter_description",
+                "dsk_struct_sort_field" : "counter_id",
+                "dsk_to_mem_function" : "am_dsk_counter_description_to_mem"
+            },
+        },
+        "to_mem_copy_fields" : ["counter_id", "name"]
+    },
+
+    "am_dsk_counter_event" : {
+        "type_id" : "AM_FRAME_TYPE_COUNTER_EVENT",
+        "assert" : False,
+        "entity" : "counter sample",
+        "defs" : ["dsk_to_mem_copy_function"] + __default_defs,
+        "fields" : event_frame_fields + [
+            {"name" : "counter_id",
+             "type" : "uint32_t",
+             "comment" : "Numerical ID of the counter this sample is for"},
+            {"name" : "time",
+             "type" : "uint64_t",
+             "comment" : "Timestamp of the sample" },
+            {"name" : "value",
+             "type" : "int64_t",
+             "comment" : "Value of the counter sample" }],
+        "process" : {
+            "type" : "per_event_collection_per_id_timestamp",
+            "args" : {
+                "ecoll_array_type_id" : "AM_EVENT_ARRAY_TYPE_COUNTER_EVENT_ARRAY_COLLECTION",
+                "ecoll_array_struct_name" : "am_counter_event_array_collection",
+                "id_array_struct_name" : "am_counter_event_array",
+                "dsk_struct_ecoll_id_field" : "collection_id",
+                "dsk_struct_id_field" : "counter_id",
+                "dsk_struct_timestamp_field" : "time",
+                "mem_struct_name" : "am_counter_event",
+                "mem_struct_timestamp_field" : "time",
+                "dsk_to_mem_function" : "am_dsk_counter_event_to_mem"
+            }
+        },
+        "to_mem_copy_fields" : ["time", "value"]
+    },
+
+    "am_dsk_event_collection" : {
+        "type_id" : "AM_FRAME_TYPE_EVENT_COLLECTION",
+        "assert" : False,
+        "entity" : "event collection",
+        "fields" : global_frame_fields + [
+            {"name" : "id",
+             "type" : "uint32_t",
+             "comment" : "Numerical ID of this event collection"},
+            {"name" : "name",
+             "type" : "am_dsk_string",
+             "comment" : "Name of this event collection"}]
+    },
+
+    "am_dsk_measurement_interval" : {
+        "type_id" : "AM_FRAME_TYPE_MEASUREMENT_INTERVAL",
+        "assert" : False,
+        "entity" : "measurement interval",
+        "defs" : ["dsk_to_mem_copy_function"] + __default_defs,
+        "fields" : global_frame_fields + [
+            {"name" : "interval",
+             "type" : "am_dsk_interval",
+             "comment" : "Start and end of the measurement interval"}],
+        "process" : {
+            "type" : "per_trace_array",
+            "args" : {
+                "trace_array_field" : "measurement_intervals",
+                "trace_array_struct_name" : "am_measurement_interval_array",
+                "mem_struct_name" : "am_measurement_interval",
+                "dsk_struct_sort_field" : "interval.start",
+                "dsk_to_mem_function" : "am_dsk_measurement_interval_to_mem"
+            },
+        },
+        "to_mem_copy_fields" : ["interval"]
+    },
+
+    "am_dsk_event_mapping" : {
+        "type_id" : "AM_FRAME_TYPE_EVENT_MAPPING",
+        "assert" : False,
+        "entity" : "event mapping",
+        "fields" : global_frame_fields + [
+            {"name" : "collection_id",
+             "type" : "uint32_t",
+             "comment" : "Numerical ID of this event collection"},
+
+            {"name" : "hierarchy_id",
+             "type" : "uint32_t",
+             "comment" : "Numerical ID of the hierarchy for the mapping"},
+
+            {"name" : "node_id",
+             "type" : "uint32_t",
+             "comment" : "Numerical ID of the hierarchy node for the mapping"},
+
+            {"name" : "interval",
+             "type" : "am_dsk_interval",
+             "comment" : "Interval during which the event collection was associated to the node" }
+        ]
+    }
+}
+
+aux_types = {
+    "am_dsk_string" : {
+        "needs_constructor" : True,
+        "is_frame" : False,
+        "entity" : "string",
+        "defs" : ["destructor", "type"],
+        "fields" : [
+            {"name" : "len",
+             "type" : "uint32_t",
+             "comment" : "Length of the string"},
+            {"name" : "str",
+             "type" : "char*",
+             "comment" : "Characters of the string (not zero-terminated)"}
+        ]
+    },
+
+    "am_dsk_interval" : {
+        "comment" : "An interval with a start and end timestamp.",
+        "is_frame" : False,
+        "defs" : ["dsk_to_mem_copy_function"] + __default_defs,
+        "entity" : "interval",
+        "fields" : [
+            {"name" : "start",
+             "type" : "uint64_t",
+             "comment" : "Start of the interval"},
+            {"name" : "end",
+             "type" : "uint64_t",
+             "comment" : "End of the interval"}
+        ],
+        "to_mem_copy_fields" : ["start", "end"]
+    }
+}
+
+def __expand_types(types):
+    ret = {}
+
+    for tname in types.keys():
+        t = types[tname]
+        t = am_type_aux.dict_set_default(t, __default_fields)
+        types[tname] = t
+        t["name"] = tname
+
+    for tname in am_type_aux.topological_sort(types):
+        t = types[tname]
+
+        if t["compound"]:
+            if not("c_type" in t.keys()):
+                t["c_type"] = "struct "+tname
+
+            for field in t["fields"]:
+                field_type = am_type_aux.dicts_find(field["type"], [types, am_base_types.types])
+
+                if field_type["destructor"]:
+                    if not "destructor" in t["defs"]:
+                        t["defs"].append("destructor")
+
+                    t["destructor"] = tname+"_destroy"
+                    break
+
+        ret[tname] = t
+
+    return ret
+
+def find(tname):
+    return am_type_aux.dicts_find(tname, [types, am_base_types.types])
+
+def find_same_mem(mem_name):
+    # Integer types have the same name, without prefix
+    if am_type_aux.isinttype(mem_name):
+        return find(mem_name)
+
+    if not mem_name.startswith("am_"):
+        raise Exception("Unexpected type name "+mem_name+". Expected type starting with am_")
+
+    return find("am_dsk_"+mem_name[3:])
+
+types = __expand_types(am_type_aux.merge_dicts(header, frames, aux_types))
---
+++ refactor/libaftermath-core/src/defs/am_mem_types.py
@@ -0,0 +1,155 @@
+#!/usr/bin/env python
+
+# Author: Andi Drebes <andi.drebes@lip6.fr>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301,
+# USA.
+
+import am_type_aux
+import am_base_types
+
+# Definition of in-memory data structures. See doc/type_definitions for
+# documentation.
+
+__default_fields = {
+    "compound" : True,
+    "destructor" : None,
+    "needs_constructor" : False,
+    "defs" : ["type"]
+}
+
+types = {
+    "am_string" : {
+        "compound" : False,
+        "entity" : "string",
+        "defs" : [],
+        "c_type" : "char*",
+        "destructor" : "am_free_charp",
+        "needs_constructor" : True
+    },
+
+    "am_interval" : {
+        "comment" : "An interval with a start and end timestamp.",
+        "entity" : "interval",
+        "fields" : [
+            {"name" : "start",
+             "type" : "am_timestamp_t",
+             "comment" : "Start of the interval"},
+            {"name" : "end",
+             "type" : "am_timestamp_t",
+             "comment" : "End of the interval"}
+        ]
+    },
+
+    "am_state_description": {
+        "comment" : "A description associating a name with a numerical state ID",
+        "entity" : "state description",
+        "fields" : [
+            {"name" : "state_id",
+             "type" : "am_state_t",
+             "comment" : "ID of the state this description is for"},
+            {"name" : "name",
+             "type" : "am_string",
+             "comment" : "Name of the state"}]},
+
+    "am_counter_description" : {
+        "comment" : "A description associating a name with a numerical counter ID",
+        "entity" : "counter description",
+        "fields" : [
+            {"name" : "counter_id",
+             "type" : "am_counter_t",
+             "comment" : "ID of the counter this description is for"},
+            {"name" : "name",
+             "type" : "am_string",
+             "comment" : "Name of the counter"}]},
+
+    "am_state_event": {
+        "comment" : "A state (e.g., a run-time state)",
+        "entity" : "state",
+        "fields" : [
+            {"name" : "interval",
+             "type" : "am_interval",
+             "comment" : "Interval during which the state was active"},
+            {"name" : "state",
+             "type" : "am_state_t",
+             "comment" : "ID of the state"}
+        ]
+    },
+
+    "am_counter_event": {
+        "comment" : "A counter event",
+        "entity" : "state",
+        "fields" : [
+            {"name" : "time",
+             "type" : "am_timestamp_t",
+             "comment" : "Timestamp of the counter interval"},
+            {"name" : "value",
+             "type" : "am_counter_value_t",
+             "comment" : "Value of the counter event"}
+        ]
+    },
+
+    "am_measurement_interval": {
+        "comment" : "A measurment interval",
+        "entity" : "measurement interval",
+        "fields" : [
+            {"name" : "interval",
+             "type" : "am_interval",
+             "comment" : "Start and end of the measurement interval"}
+        ]
+    }
+}
+
+def __expand_types(types):
+    ret = {}
+
+    for tname in types.keys():
+        t = types[tname]
+        t = am_type_aux.dict_set_default(t, __default_fields)
+        types[tname] = t
+
+    for tname in types.keys():
+        t = types[tname]
+        t["name"] = tname
+
+        if t["compound"]:
+            if not("c_type" in t.keys()):
+                t["c_type"] = "struct "+tname
+
+            for field in t["fields"]:
+                if find(field["type"])["destructor"]:
+                    if not "destructor" in t["defs"]:
+                        t["defs"].append("destructor")
+
+                    t["destructor"] = tname+"_destroy"
+                    break
+
+        ret[tname] = t
+
+    return ret
+
+def find(tname):
+    return am_type_aux.dicts_find(tname, [types, am_base_types.types])
+
+def find_same_dsk(dsk_name):
+    # Integer types have the same name, without prefix
+    if am_type_aux.isinttype(dsk_name):
+        return find(dsk_name)
+
+    if not dsk_name.startswith("am_dsk_"):
+        raise Exception("Unexpected type name "+dsk_name+". Expected type starting with am_dsk_")
+
+    return find("am_"+dsk_name[7:])
+
+types = __expand_types(types)
