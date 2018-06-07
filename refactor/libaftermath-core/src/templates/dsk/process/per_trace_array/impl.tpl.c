e708c399cf30d900d8e429a7ca9ce4b86011710f
Andi Drebes <andi@drebesium.org>
2018-06-07 13:15:55 +0200

    Libaftermath-core: Per trace array processor: Do not sort entries by default

--- refactor/libaftermath-core/src/templates/dsk/process/per_trace_array/impl.tpl.c
+++ refactor/libaftermath-core/src/templates/dsk/process/per_trace_array/impl.tpl.c
@@ -6,10 +6,19 @@
 	struct {{pargs.trace_array_struct_name}}* a = &ctx->trace->{{pargs.trace_array_field}};
 	{{memtype.c_type}}* mem;
 
+	{% if pargs.dsk_struct_sort_field %}
 	if(!(mem = {{pargs.trace_array_struct_name}}_reserve_sorted(a, f->{{pargs.dsk_struct_sort_field}}))) {
 		AM_IOERR_GOTO_NA(ctx, out_err, AM_IOERR_ALLOC,
 				 "Could not allocate space for a {{memtype.entity}}.");
 	}
+	{% else %}
+	if({{pargs.trace_array_struct_name}}_reserve_end(a)) {
+		AM_IOERR_GOTO_NA(ctx, out_err, AM_IOERR_ALLOC,
+				 "Could not allocate space for a {{memtype.entity}}.");
+	}
+
+	mem = &a->elements[a->num_elements-1];
+	{% endif %}
 
 	if({{pargs.dsk_to_mem_function}}(ctx, f, mem)) {
 		AM_IOERR_GOTO_NA(ctx, out_err_assign, AM_IOERR_CONVERT,
