15731335fc6bbb0dbc9f6ea8bcab5b95cd33e6b9
Andi Drebes <andi@drebesium.org>
2018-09-13 18:14:10 +0200

    Libaftermath-core: Add macro am_trace_for_each_event_collection_subarray

--- refactor/libaftermath-core/src/trace.h
+++ refactor/libaftermath-core/src/trace.h
@@ -201,4 +201,181 @@ static inline int am_event_collection_array_iter_valid(
 	    am_event_collection_array_iter_valid(&(iter));		\
 	    (iter) = am_event_collection_array_iter_next(t, ident, &(iter)))
 
+/* Helper data structure for iterating over per-event-collection sub-arrays. */
+struct am_event_collection_subarray_iter {
+	struct am_event_collection* ecoll;
+	struct am_typed_array_generic* arr;
+	struct am_typed_array_generic* subarr;
+};
+
+/* Returns an iterator for the next per-event-collection sub-array after the one
+ * identified by prev. If prev is NULL, an iterator to the first sub-array is
+ * returned. If no further sub-array exists, the values of the iterator are set
+ * to NULL.
+ *
+ * If ecoll is non-NULL, the value of the event collection of the iterator is
+ * assigned to *ecoll.
+ *
+ * If arr is non-NULL, the value of the array pointer of the iterator is
+ * assigned to *arr as a generic typed array.
+ *
+ * If subarr is non-NULL, the value of the sub-array pointer of the iterator is
+ * assigned to *subarr as a generic typed array.
+ */
+static inline struct am_event_collection_subarray_iter
+am_event_collection_subarray_iter_next_assign(
+	struct am_trace* t,
+	const char* ident,
+	size_t subarr_size,
+	struct am_event_collection_subarray_iter* prev,
+	struct am_event_collection** ecoll,
+	struct am_typed_array_generic** arr,
+	struct am_typed_array_generic** subarr)
+{
+	struct am_event_collection_subarray_iter ret;
+
+	if(!prev) {
+		ret.ecoll = NULL;
+		ret.arr = NULL;
+		ret.subarr = NULL;
+	} else {
+		ret = *prev;
+	}
+
+	for(ret.ecoll = (!ret.ecoll) ? t->event_collections.elements : ret.ecoll;
+	    am_event_collection_array_is_element_ptr(&t->event_collections,
+						     ret.ecoll);
+	    ret.ecoll++, ret.arr = NULL, ret.subarr = NULL)
+	{
+		/* Find the per-event-collection array; If we resume a previous
+		 * iterator, just skip to the subarr portion
+		 */
+		if(!ret.arr) {
+			ret.arr = (struct am_typed_array_generic*)
+				am_event_collection_find_event_array(ret.ecoll,
+								     ident);
+
+			/* If this event collection does not have a
+			 * per-event-collection array of the specified type,
+			 * move on to the next collection */
+			if(!ret.arr)
+				continue;
+		}
+
+		ret.subarr = (struct am_typed_array_generic*)
+			((!ret.subarr) ?
+			 ret.arr->elements :
+			 AM_PTR_ADD(ret.subarr, subarr_size));
+
+		if(AM_PTR_LESS(ret.subarr,
+			       AM_PTR_ADD(ret.arr->elements,
+					  ret.arr->num_elements * subarr_size)))
+		{
+			return ret;
+		}
+	}
+
+	ret.ecoll = NULL;
+	ret.arr = NULL;
+	ret.subarr = NULL;
+
+	if(ecoll)
+		*ecoll = ret.ecoll;
+
+	if(arr)
+		*arr = ret.arr;
+
+	if(subarr)
+		*subarr = ret.subarr;
+
+	return ret;
+}
+
+/* Returns an iterator for the next per-event-collection sub-array after the one
+ * identified by prev. If prev is NULL, an iterator to the first sub-array is
+ * returned. If no further sub-array exists, the values of the iterator are set
+ * to NULL.
+ */
+static inline struct am_event_collection_subarray_iter
+am_event_collection_subarray_iter_next(
+	struct am_trace* t,
+	const char* ident,
+	size_t subarr_size,
+	struct am_event_collection_subarray_iter* prev)
+{
+	return am_event_collection_subarray_iter_next_assign(
+		t, ident, subarr_size, prev, NULL, NULL, NULL);
+}
+
+/* Returns an iterator to the first per-event-collection sub-array identified by
+ * ident. If no such sub-array exists, the values of the iterator are set to
+ * NULL.
+ *
+ * If ecoll is non-NULL, the value of the event collection of the iterator is
+ * assigned to *ecoll.
+ *
+ * If arr is non-NULL, the value of the array pointer of the iterator is
+ * assigned to *arr as a generic typed array.
+ *
+ * If subarr is non-NULL, the value of the sub-array pointer of the iterator is
+ * assigned to *subarr as a generic typed array.
+ */
+static inline struct am_event_collection_subarray_iter
+am_event_collection_subarray_iter_first_assign(
+	struct am_trace* t,
+	const char* ident,
+	size_t subarr_size,
+	struct am_event_collection** ecoll,
+	struct am_typed_array_generic** arr,
+	struct am_typed_array_generic** subarr)
+{
+	return am_event_collection_subarray_iter_next_assign(
+		t, ident, subarr_size, NULL, ecoll, arr, subarr);
+}
+
+/* Returns an iterator to the first per-event-collection sub-array identified by
+ * ident. If no such sub-array exists, the values of the iterator are set to
+ * NULL.
+ */
+static inline struct am_event_collection_subarray_iter
+am_event_collection_subarray_iter_first(struct am_trace* t,
+					const char* ident,
+					size_t subarr_size)
+{
+	return am_event_collection_subarray_iter_first_assign(
+		t, ident, subarr_size, NULL, NULL, NULL);
+}
+
+/* Returns true if iter is a valid event collection subarray iterator that may
+ * be dereferenced, otherwise false.
+ */
+static inline int am_event_collection_subarray_iter_valid(
+	struct am_event_collection_subarray_iter* iter)
+{
+	return iter->ecoll && iter->arr && iter->subarr;
+}
+
+/* Iterates over all event collection sub-arrays belonging to a
+ * per-event-collection array whose identifier is ident. At each iteration, iter
+ * is assigned a pointer to the sub-array.
+ */
+#define am_trace_for_each_event_collection_subarray(t, ident, subarr_type, iter)\
+	for(struct am_event_collection_subarray_iter __it =			\
+		    am_event_collection_subarray_iter_first_assign(		\
+			    t, ident, sizeof(subarr_type), NULL, NULL,		\
+			    (struct am_typed_array_generic**)&iter);		\
+	    am_event_collection_subarray_iter_valid(&__it);			\
+	    __it = am_event_collection_subarray_iter_next_assign(		\
+			t, ident, sizeof(subarr_type), &__it, NULL, NULL,	\
+				(struct am_typed_array_generic**)&iter))
+
+/* Iterates over all event collection sub-arrays belonging to a
+ * per-event-collection array whose identifier is ident using a struct
+ * am_event_collection_subarray_iter.
+ */
+#define am_trace_iter_each_event_collection_subarray(t, ident, iter)	\
+	for((iter) = am_event_collection_subarray_iter_first(t, ident); \
+	    am_event_collection_subarray_iter_valid(&(iter));		\
+	    (iter) = am_event_collection_subarray_iter_next(t, ident, &(iter)))
+
 #endif
