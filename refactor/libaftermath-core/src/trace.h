375c6e88ca91f42a63a9c0e4e2eceae4fa3e594c
Andi Drebes <andi@drebesium.org>
2018-09-13 15:27:43 +0200

    Libaftermath-core: Add macro am_trace_for_each_event_collection_array

--- refactor/libaftermath-core/src/trace.h
+++ refactor/libaftermath-core/src/trace.h
@@ -73,4 +73,132 @@ void* am_trace_find_or_add_trace_array(struct am_trace* t, const char* type);
 	  };									\
 	    __it.arr && __it.idx < __it.arr->num_elements;			\
 	    __it.idx++, (iter) = &((typeof(iter))__it.arr->elements)[__it.idx])
+
+/* Helper data structure for iterating over per-event-collection arrays. */
+struct am_event_collection_array_iter {
+	struct am_event_collection* ecoll;
+	struct am_typed_array_generic* arr;
+};
+
+/* Returns an iterator for the next per-event-collection array after the one
+ * identified by prev. If prev is NULL, an iterator to the first array is
+ * returned. If no further array exists, the values of the iterator are set to
+ * NULL.
+ *
+ * If ecoll is non-NULL, the value of the event collection of the iterator is
+ * assigned to *ecoll.
+ *
+ * If arr is non-NULL, the value of the array pointer of the iterator is
+ * assigned to *arr as a generic typed array.
+ */
+static inline struct am_event_collection_array_iter
+am_event_collection_array_iter_next_assign(
+	struct am_trace* t,
+	const char* ident,
+	struct am_event_collection_array_iter* prev,
+	struct am_event_collection** ecoll,
+	struct am_typed_array_generic** arr)
+{
+	struct am_event_collection_array_iter ret;
+
+	for(ret.ecoll = (prev) ? (prev->ecoll+1) : t->event_collections.elements;
+	    am_event_collection_array_is_element_ptr(&t->event_collections,
+						     ret.ecoll);
+	    ret.ecoll++)
+	{
+		ret.arr = (struct am_typed_array_generic*)
+			am_event_collection_find_event_array(ret.ecoll, ident);
+
+		if(ret.arr)
+			return ret;
+	}
+
+	ret.ecoll = NULL;
+	ret.arr = NULL;
+
+	if(ecoll)
+		*ecoll = ret.ecoll;
+
+	if(arr)
+		*arr = ret.arr;
+
+	return ret;
+}
+
+/* Returns an iterator for the next per-event-collection array after the one
+ * identified by prev. If prev is NULL, an iterator to the first array is
+ * returned. If no further array exists, the values of the iterator are set to
+ * NULL.
+ */
+static inline struct am_event_collection_array_iter
+am_event_collection_array_iter_next(
+	struct am_trace* t,
+	const char* ident,
+	struct am_event_collection_array_iter* prev)
+{
+	return am_event_collection_array_iter_next_assign(
+		t, ident, prev, NULL, NULL);
+}
+
+/* Returns an iterator to the first per-event-collection array identified by
+ * ident. If no such array exists, the values of the iterator are set to
+ * NULL.
+ *
+ * If ecoll is non-NULL, the value of the event collection of the iterator is
+ * assigned to *ecoll.
+ *
+ * If arr is non-NULL, the value of the array pointer of the iterator is
+ * assigned to *arr as a generic typed array.
+ */
+static inline struct am_event_collection_array_iter
+am_event_collection_array_iter_first_assign(
+	struct am_trace* t,
+	const char* ident,
+	struct am_event_collection** ecoll,
+	struct am_typed_array_generic** arr)
+{
+	return am_event_collection_array_iter_next_assign(
+		t, ident, NULL, ecoll, arr);
+}
+
+/* Returns an iterator to the first per-event-collection array identified by
+ * ident. If no such array exists, the values of the iterator are set to
+ * NULL.
+ */
+static inline struct am_event_collection_array_iter
+am_event_collection_array_iter_first(struct am_trace* t, const char* ident)
+{
+	return am_event_collection_array_iter_first_assign(t, ident, NULL, NULL);
+}
+
+/* Returns true if iter is a valid event collection array iterator that may be
+ * dereferenced, otherwise false.
+ */
+static inline int am_event_collection_array_iter_valid(
+	struct am_event_collection_array_iter* iter)
+{
+	return iter->ecoll && iter->arr;
+}
+
+/* Iterates over all event collection arrays whose array identifier is ident. At
+ * each iteration, iter is assigned a pointer to the array.
+ */
+#define am_trace_for_each_event_collection_array(t, ident, iter)	\
+	for(struct am_event_collection_array_iter __it =		\
+		    am_event_collection_array_iter_first_assign(	\
+			    t, ident, NULL,				\
+			    (struct am_typed_array_generic**)&iter);	\
+	    am_event_collection_array_iter_valid(&__it);		\
+	    __it = am_event_collection_array_iter_next_assign(		\
+		    t, ident, &__it, NULL,				\
+		    (struct am_typed_array_generic**)&iter))
+
+/* Iterates over all event collection arrays whose array identifier is ident
+ * using a struct am_event_collection_array_iter.
+ */
+#define am_trace_iter_each_event_collection_array(t, ident, iter)	\
+	for((iter) = am_event_collection_array_iter_first(t, ident);	\
+	    am_event_collection_array_iter_valid(&(iter));		\
+	    (iter) = am_event_collection_array_iter_next(t, ident, &(iter)))
+
 #endif
