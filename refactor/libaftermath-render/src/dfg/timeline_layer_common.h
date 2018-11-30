8da740f0411bab014406ec4bb44601a2ca64dbf8
Andi Drebes <andi@drebesium.org>
2018-11-30 17:57:32 +0100

    Libaftermath-render: DFG: Add macros for functions updating layer parameters

--- refactor/libaftermath-render/src/dfg/timeline_layer_common.h
+++ refactor/libaftermath-render/src/dfg/timeline_layer_common.h
@@ -19,8 +19,11 @@
 #ifndef AM_RENDER_DFG_TIMELINE_LAYER_COMMON_H
 #define AM_RENDER_DFG_TIMELINE_LAYER_COMMON_H
 
+#include <aftermath/core/ansi_extras.h>
 #include <aftermath/core/dfg_type.h>
 #include <aftermath/core/dfg_node.h>
+#include <aftermath/render/cairo_extras.h>
+#include <limits.h>
 
 /* Declares a DFG type "const am::render::timeline::layer::<ident_name>*" for
  * pointers to a render layer */
@@ -115,5 +118,124 @@
 										\
 		return 0;							\
 	}
+
+#define AM_RENDER_DFG_DECL_LAYER_VALUPD_FUN(SUFFIX, TIN, TOUT, REASSIGN_EXPR)	\
+	/* Updates the value of type TOUT at offset tgt_offset of each layer	\
+	 * read from layers_port according to the last value of type TIN read	\
+	 * from in_port. The assignment is done using REASSIGN_EXPR, which takes\
+	 * a pointer to the layer's value, a pointer to the input value and a	\
+	 * pointer to an integer that allows the expression to indicate whether \
+	 * the conversion was successful (0) or failed (1).			\
+	 *									\
+	 * If any layer was updated, *changed is set to 1, otherwise remains	\
+	 * untouched.								\
+	 *									\
+	 * Returns 0 on success, otherwise 1.					\
+	 */									\
+	static inline int am_render_dfg_valupd_##SUFFIX(			\
+		struct am_dfg_port* layers_port,				\
+		struct am_dfg_port* in_port,					\
+		off_t tgt_offset,						\
+		int* changed)							\
+	{									\
+		void** layers = layers_port->buffer->data;			\
+		size_t num_layers = layers_port->buffer->num_samples;		\
+		TIN* v;							\
+		TOUT* vout;							\
+		int assign_ret;						\
+										\
+		if(am_dfg_port_activated(in_port) &&				\
+		   in_port->buffer->num_samples > 0)				\
+		{								\
+			/* Skip intermediate changes, since they wouldn't be	\
+			 * visible anyways; just take the last sample */	\
+			if(am_dfg_buffer_last_ptr(in_port->buffer, (void**)&v)) \
+				return 1;					\
+										\
+			for(size_t i = 0; i < num_layers; i++) {		\
+				vout = AM_PTR_ADD(layers[i], tgt_offset);	\
+										\
+				REASSIGN_EXPR(vout, v, &assign_ret);		\
+										\
+				if(assign_ret != 0)				\
+					return 1;				\
+			}							\
+										\
+			*changed = 1;						\
+		}								\
+										\
+		return 0;							\
+	}
+
+/* REASSIGN_EXPR for AM_RENDER_DFG_DECL_LAYER_VALUPD_FUN for values of identical
+ * type */
+#define AM_RENDER_DFG_LAYER_VALUPD_DIRECT_REASSIGN_EXPR(PTGT, PSRC, PRETVAL)	\
+	do {									\
+		*(PTGT) = *(PSRC);						\
+		*(PRETVAL) = 0;						\
+	} while(0)
+
+/* REASSIGN_EXPR for AM_RENDER_DFG_DECL_LAYER_VALUPD_FUN for a uint64_t assigned
+ * to a size_t */
+#define AM_RENDER_DFG_LAYER_VALUPD_SIZE_T_FROM_UINT64_REASSIGN_EXPR(	\
+	PTGT, PSRC, PRETVAL)						\
+	do {								\
+		if(am_safe_size_from_u64(PTGT, *(PSRC)))		\
+			*(PRETVAL) = 1;				\
+		else							\
+			*(PRETVAL) = 0;				\
+	} while(0)
+
+/* REASSIGN_EXPR for AM_RENDER_DFG_DECL_LAYER_VALUPD_FUN for a uint64_t assigned
+ * to a unsigned int */
+#define AM_RENDER_DFG_LAYER_VALUPD_UINT_FROM_UINT64_REASSIGN_EXPR(	\
+	PTGT, PSRC, PRETVAL)						\
+	do {								\
+		if((*PSRC) > UINT_MAX) {				\
+			*(PRETVAL) = 1;				\
+		} else {						\
+			(*PTGT) = (*PSRC);				\
+			*(PRETVAL) = 0;				\
+		}							\
+	} while(0)
+
+/* REASSIGN_EXPR for AM_RENDER_DFG_DECL_LAYER_VALUPD_FUN for a string value
+ * assigned to an already allocated string */
+#define AM_RENDER_DFG_LAYER_VALUPD_STRING_REASSIGN_EXPR(PTGT, PSRC, PRETVAL)	\
+	do {									\
+		char* tmp = strdup(*(PSRC));					\
+										\
+		if(tmp) {							\
+			free((*PTGT));						\
+			*(PTGT) = tmp;						\
+			*(PRETVAL) = 0;					\
+		} else {							\
+			*(PRETVAL) = 1;					\
+		}								\
+	} while(0)
+
+AM_RENDER_DFG_DECL_LAYER_VALUPD_FUN(
+	rgba, struct am_rgba, struct am_rgba,
+	AM_RENDER_DFG_LAYER_VALUPD_DIRECT_REASSIGN_EXPR)
+
+AM_RENDER_DFG_DECL_LAYER_VALUPD_FUN(
+	double, double, double,
+	AM_RENDER_DFG_LAYER_VALUPD_DIRECT_REASSIGN_EXPR)
+
+AM_RENDER_DFG_DECL_LAYER_VALUPD_FUN(
+	bool, int, int,
+	AM_RENDER_DFG_LAYER_VALUPD_DIRECT_REASSIGN_EXPR)
+
+AM_RENDER_DFG_DECL_LAYER_VALUPD_FUN(
+	size_t, uint64_t, size_t,
+	AM_RENDER_DFG_LAYER_VALUPD_SIZE_T_FROM_UINT64_REASSIGN_EXPR)
+
+AM_RENDER_DFG_DECL_LAYER_VALUPD_FUN(
+	uint, uint64_t, unsigned int,
+	AM_RENDER_DFG_LAYER_VALUPD_UINT_FROM_UINT64_REASSIGN_EXPR)
+
+AM_RENDER_DFG_DECL_LAYER_VALUPD_FUN(
+	string, char*, char*,
+	AM_RENDER_DFG_LAYER_VALUPD_STRING_REASSIGN_EXPR)
 
 #endif
