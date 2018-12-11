7103f7353d490bf302d4abd970c8be49563c1be9
Andi Drebes <andi@drebesium.org>
2018-12-11 14:28:49 +0100

    Libaftermath-render: DFG: Add DFG node configuring state layer

--- refactor/libaftermath-render/src/dfg/nodes/timeline/layers/state.h
+++ refactor/libaftermath-render/src/dfg/nodes/timeline/layers/state.h
@@ -27,7 +27,27 @@ AM_RENDER_DFG_DECL_TIMELINE_LAYER_FILTER_NODE_TYPE(
 	"state",
 	"Timeline State Layer Filter")
 
+int am_render_dfg_timeline_state_layer_configuration_node_process(
+	struct am_dfg_node* n);
+
+AM_DFG_DECL_BUILTIN_NODE_TYPE(
+	am_render_dfg_timeline_state_layer_configuration_node_type,
+	"am::render::timeline::layer::state::configuration",
+	"Timeline State Layer Configuration",
+	AM_DFG_NODE_DEFAULT_SIZE,
+	AM_DFG_DEFAULT_PORT_DEPS_PURE_FUNCTIONAL,
+	AM_DFG_NODE_FUNCTIONS({
+		.process = am_render_dfg_timeline_state_layer_configuration_node_process
+	}),
+	AM_DFG_NODE_PORTS(
+		{ "layer", "const am::render::timeline::layer::state*", AM_DFG_PORT_IN },
+		{ "enable", "am::core::bool", AM_DFG_PORT_IN }
+	),
+	AM_DFG_PORT_DEPS(),
+	AM_DFG_NODE_PROPERTIES())
+
 AM_DFG_ADD_BUILTIN_NODE_TYPES(
-	&am_render_dfg_timeline_state_layer_filter_node_type)
+	&am_render_dfg_timeline_state_layer_filter_node_type,
+	&am_render_dfg_timeline_state_layer_configuration_node_type)
 
 #endif
