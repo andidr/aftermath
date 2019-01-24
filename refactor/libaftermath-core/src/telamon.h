5902b514204b4591485cf71b25ca480f18969121
Andi Drebes <andi@drebesium.org>
2019-01-24 15:18:39 +0100

    Libaftermath-core: Add function for tree depth of a Telamon candidate tree

--- refactor/libaftermath-core/src/telamon.h
+++ refactor/libaftermath-core/src/telamon.h
@@ -19,6 +19,7 @@
 #ifndef AM_TELAMON_H
 #define AM_TELAMON_H
 
+#include <aftermath/core/dfs.h>
 #include <aftermath/core/in_memory.h>
 #include <stdlib.h>
 
@@ -64,6 +65,35 @@ size_t am_telamon_candidate_child_idx(const struct am_telamon_candidate* c,
 			return i;
 
 	return SIZE_MAX;
+}
+
+/* Internal callback function for am_dfs_norec_telamon_candidate_depth */
+void am_telamon_depth_dfs_callback(const struct am_telamon_candidate* node,
+				   size_t depth,
+				   void* data);
+
+/* Define an iterative depth-first search function to determine the depth of a
+ * tree rooted at a candidate */
+AM_DECL_DFS_FUNCTION(_telamon_candidate_depth,
+		     const struct am_telamon_candidate,
+		     size_t*,
+		     am_telamon_candidate_parent,
+		     am_telamon_candidate_nth_child,
+		     am_telamon_candidate_is_last_child,
+		     am_telamon_candidate_child_idx,
+		     am_telamon_candidate_has_children,
+		     am_telamon_depth_dfs_callback)
+
+/* Returns the depth of the candidate tree rooted at n using an iterative
+ * method. The root itself is at depth 1. */
+static inline size_t
+am_telamon_candidate_tree_depth(const struct am_telamon_candidate* n)
+{
+	size_t depth = 0;
+
+	am_dfs_norec_telamon_candidate_depth(n, 20, &depth);
+
+	return depth + 1;
 }
 
 #endif
