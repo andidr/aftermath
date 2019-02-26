a42327334cf0d31fb9b2a3bbf673619612ac3e42
Andi Drebes <andi@drebesium.org>
2019-02-26 11:53:32 +0100

    Aftermath QT GUI: Add list model for widget creators

---
+++ refactor/aftermath/src/models/WidgetTypeListModel.cpp
@@ -0,0 +1,119 @@
+/**
+ * Author: Andi Drebes <andi@drebesium.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as published
+ * by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301,
+ * USA.
+ */
+
+#include "WidgetTypeListModel.h"
+#include <QtAlgorithms>
+
+WidgetTypeListModel::WidgetTypeListModel(
+	const GUIFactory* factory, QObject *parent) :
+	QAbstractTableModel(parent), factory(factory)
+{
+	for(auto creator: this->factory->getWidgetCreators())
+		this->filteredCreators.push_back(creator);
+
+	qSort(this->filteredCreators.begin(),
+	      this->filteredCreators.end(),
+	      WidgetCreatorComparator());
+}
+
+int WidgetTypeListModel::rowCount(const QModelIndex& parent) const
+{
+	return this->filteredCreators.size();
+}
+
+int WidgetTypeListModel::columnCount(const QModelIndex& parent) const
+{
+	return 1;
+}
+
+/* Returns the row-th creator. If the row index exceeds the maximum index for
+ * the list of types, NULL is returned.
+ */
+WidgetCreator* WidgetTypeListModel::getWidgetCreator(int row)
+{
+	if(row < 0 || row >= this->filteredCreators.size())
+		return NULL;
+
+	return filteredCreators[row];
+}
+
+/* Returns the string as a QVariant for the specified index. Only the display
+ * role is implemented.
+ */
+QVariant WidgetTypeListModel::data(const QModelIndex& index, int role)
+	const
+{
+	int row = index.row();
+	int column = index.column();
+
+	if(role == Qt::DisplayRole) {
+		if(row < 0 ||
+		   row >= this->filteredCreators.size() ||
+		   column >= 2)
+		{
+			return QVariant();
+		}
+
+		if(index.column() == 0)
+			return QString(this->filteredCreators[row]->getGroupName().c_str());
+	}
+
+	return QVariant();
+}
+
+/* Provides the name of the section-th column (first column: the creator's group
+ * name). Only the display role is implemented and only for the horizontal
+ * orientation.
+ */
+QVariant WidgetTypeListModel::headerData(int section,
+					 Qt::Orientation orientation,
+					 int role) const
+{
+	if(role == Qt::DisplayRole) {
+		if(orientation == Qt::Horizontal) {
+			if(section == 0)
+				return QString("Name");
+		}
+	}
+
+	return QVariant();
+}
+
+/* Filters the list of creators with a string str. The set of creators is
+ * limited to creators whose group name containts the substring str. The
+ * parameter sens indicates whether the search for the substring should be case
+ * sensistive or case insensitive.
+ */
+void WidgetTypeListModel::setFilterString(
+	const QString& str, enum Qt::CaseSensitivity sens)
+{
+	this->filteredCreators.clear();
+
+	for(auto creator: this->factory->getWidgetCreators()) {
+		const QString gname(creator->getGroupName().c_str());
+
+		if(gname.indexOf(str, 0, sens) != -1)
+			this->filteredCreators.push_back(creator);
+	}
+
+	qSort(this->filteredCreators.begin(),
+	      this->filteredCreators.end(),
+	      WidgetCreatorComparator());
+
+	emit layoutChanged();
+}
---
+++ refactor/aftermath/src/models/WidgetTypeListModel.h
@@ -0,0 +1,64 @@
+/**
+ * Author: Andi Drebes <andi@drebesium.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as published
+ * by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301,
+ * USA.
+ */
+
+#ifndef AM_WIDGETTYPELISTMODEL_H
+#define AM_WIDGETTYPELISTMODEL_H
+
+#include "../gui/factory/GUIFactory.h"
+#include <QAbstractTableModel>
+#include <QSortFilterProxyModel>
+#include <QStringListModel>
+
+/* A table model providing read-only access to all widget types for which a
+ * widget creator has been registered at the indicated widget factory.
+ *
+ * The types might be filtered with a string, limiting the set of types to those
+ * types that contain the string as a substring.
+ */
+class WidgetTypeListModel : public QAbstractTableModel {
+	Q_OBJECT
+
+	public:
+		WidgetTypeListModel(const GUIFactory* factory,
+				    QObject* parent = Q_NULLPTR);
+
+		int rowCount(const QModelIndex& parent = QModelIndex())
+			const override;
+
+		int columnCount(const QModelIndex& parent = QModelIndex())
+			const override;
+
+		QVariant data(const QModelIndex& index,
+			      int role = Qt::DisplayRole) const override;
+
+		QVariant headerData(int section,
+				    Qt::Orientation orientation,
+				    int role) const override;
+
+		void setFilterString(
+			const QString& str,
+			enum Qt::CaseSensitivity sens = Qt::CaseSensitive);
+
+		WidgetCreator* getWidgetCreator(int row);
+
+	protected:
+		const GUIFactory* factory;
+		QVector<WidgetCreator*> filteredCreators;
+};
+
+#endif
